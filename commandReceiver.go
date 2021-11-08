2f0ae8aa747bb139582968c4ca0c52f8b7984684
snorkilator <30639243+snorkilator@users.noreply.github.com>
2021-11-08 15:27:13 -0500

    added beginnings of command Receiver sketch of program

---
+++ commandReceiver.go
@@ -0,0 +1,3 @@
+//Command receiver: takes newline split input from standard input and returns command as string
+//	^^^ this will be replaced with tcp protocol that receives commands from client's command line
+//Parser:
