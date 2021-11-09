b8edd59b2c2e181d5e4aa3e34c734473d3421cf6
snorkilator <30639243+snorkilator@users.noreply.github.com>
2021-11-09 14:47:30 -0500

    added first bit of parser

--- commandReceiver.go
+++ commandReceiver.go
@@ -9,8 +9,27 @@ import (
 func receiver() (s string, err error) {
 	reader := bufio.NewReader(os.Stdin)
 	s, err = reader.ReadString('\n')
-	s, err = reader.ReadString('\n')
 	return
+}
+
+var input = `*3\r\n$3\r\nset\r\n$1\r\na\r\n$2\r\n21\r\n`
+
+func parser(s string) (a []string, err error) {
+	if s[0] != byte('*') {
+		return nil, fmt.Errorf("input is not Array of Bulk Strings type: want *, got %v", string(s[0]))
+	}
+
+	var endofNum int
+	for i, found := 1, false; !found; i++ {
+		if s[i:i+4] == `\r\n` {
+			found = true
+			endofNum = i - 1
+		}
+	}
+	fmt.Println(s[1 : endofNum+1])
+	//find length of bulk string array
+	//number is stored between * idex 0 and first \r\n
+	return nil, nil
 }
 
 //parser
@@ -22,7 +41,7 @@ func receiver() (s string, err error) {
 // 				numerical length of array is index 1 to \r\n exclusive
 //			store length of array
 //			initialize array of strings of length of array
-//			find each element in array and store in array
+//			find each element in input array and store in new array
 //				start after first \r\n
 //				check if datatype is correct ($)
 //				find length of string
@@ -34,8 +53,9 @@ func receiver() (s string, err error) {
 //				take string and store in first empty space in array
 //
 func main() {
-	s, _ := receiver()
-	fmt.Printf("%v", s)
+	// s, _ := receiver()
+	// fmt.Printf("%v", s)
+	parser(input)
 }
 
 /*
