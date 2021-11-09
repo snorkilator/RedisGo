5c23a3b9c8a6f15f10843433835ceaa0f70521f9
snorkilator <30639243+snorkilator@users.noreply.github.com>
2021-11-08 20:09:42 -0500

    continued planning

--- commandReceiver.go
+++ commandReceiver.go
@@ -1,5 +1,46 @@
-//Command receiver: takes newline split input from standard input and returns command as string
-//	^^^ this will be replaced with tcp protocol that receives commands from client's command line
-//Implement commands: Takes string and decodes it and finds out what commands to run and runs them
-//if commands were able to be implemented, returns RESP okay to stdout
-//if functions return errors, returns error message to RESP
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func receiver() (s string, err error) {
+	reader := bufio.NewReader(os.Stdin)
+	s, err = reader.ReadString('\n')
+	s, err = reader.ReadString('\n')
+	return
+}
+
+//parser
+//		decodes RESP array, and stores in slice
+//			check datatype is orrect (*)
+//			read length of array
+//				read characters from index 1 onward
+//				until you reach \r\n
+// 				numerical length of array is index 1 to \r\n exclusive
+//			store length of array
+//			initialize array of strings of length of array
+//			find each element in array and store in array
+//				start after first \r\n
+//				check if datatype is correct ($)
+//				find length of string
+//					find everythig between $ and \r\n
+//				store as length of string
+//				extract string
+//					find first \r\n
+//					grab length of string starting after \r\n
+//				take string and store in first empty space in array
+//
+func main() {
+	s, _ := receiver()
+	fmt.Printf("%v", s)
+}
+
+/*
+command: set a 21
+(set key `a` to value `21`)
+RESP:
+*3\r\n$3\r\nset\r\n$1\r\na\r\n$2\r\n21\r\n
+*/
