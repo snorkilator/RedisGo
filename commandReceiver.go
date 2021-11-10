a0f412de31f30641fd09cd2a734b9db8d025a80b
snorkilator <30639243+snorkilator@users.noreply.github.com>
2021-11-10 08:03:00 -0500

    continued brainstorming and adding functions

--- commandReceiver.go
+++ commandReceiver.go
@@ -4,63 +4,117 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
+//receives command from client and returns it as a string
 func receiver() (s string, err error) {
 	reader := bufio.NewReader(os.Stdin)
 	s, err = reader.ReadString('\n')
 	return
 }
 
-var input = `*3\r\n$3\r\nset\r\n$1\r\na\r\n$2\r\n21\r\n`
+func responder(s string) error {
+	fmt.Println(s)
+	return nil
+}
 
-func parser(s string) (a []string, err error) {
+var input = `*4\r\n$3\r\nset\r\n$1\r\na\r\n$2\r\n21\r\n$2\r\ner\r\n`
+
+//Takes array of bulk string, and outputs a slice of strings containing the elements of the array
+func parser(s string) ([]string, error) {
+	var parsed []string
+
 	if s[0] != byte('*') {
 		return nil, fmt.Errorf("input is not Array of Bulk Strings type: want *, got %v", string(s[0]))
 	}
 
+	arrayLen, currentIndex, err := getLen(1, &s)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 0; i < arrayLen; i, currentIndex = i+1, currentIndex+4 {
+
+		if s[currentIndex] != '$' {
+			return nil, fmt.Errorf("Expected type symbol '$' for element %v in array but got %v at index %v", i+1, string(s[currentIndex]), currentIndex)
+		}
+		currentIndex++
+
+		var strLen int
+		strLen, currentIndex, err = getLen(currentIndex, &s)
+		if err != nil {
+			return nil, err
+		}
+
+		tempStr := s[currentIndex : currentIndex+strLen]
+		currentIndex += strLen
+		parsed = append(parsed, tempStr)
+		if s[currentIndex:currentIndex+4] != `\r\n` {
+			return nil, fmt.Errorf(`expected \r\n after %v but found %v starting at index %v`, parsed, s[currentIndex:currentIndex+4], currentIndex)
+		}
+	}
+	return parsed, nil
+}
+
+//Starts at some index and checks for \r\n. If it finds that, it will return a string starting at the endex and ending before the \r\n.
+//Also returns index value after \r\n
+func getLen(i int, s *string) (int, int, error) {
+	beginStr := i
 	var endofNum int
-	for i, found := 1, false; !found; i++ {
-		if s[i:i+4] == `\r\n` {
+
+	found := false
+	for ; !found; i++ {
+		if (*s)[i:i+4] == `\r\n` {
 			found = true
 			endofNum = i - 1
 		}
 	}
-	fmt.Println(s[1 : endofNum+1])
-	//find length of bulk string array
-	//number is stored between * idex 0 and first \r\n
-	return nil, nil
+	lenS := (*s)[beginStr : endofNum+1]
+
+	len, err := strconv.Atoi(lenS)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Array length is not valid number: want number but got %v", lenS)
+	}
+
+	return len, i + 3, nil
 }
 
-//parser
-//		decodes RESP array, and stores in slice
-//			check datatype is orrect (*)
-//			read length of array
-//				read characters from index 1 onward
-//				until you reach \r\n
-// 				numerical length of array is index 1 to \r\n exclusive
-//			store length of array
-//			initialize array of strings of length of array
-//			find each element in input array and store in new array
-//				start after first \r\n
-//				check if datatype is correct ($)
-//				find length of string
-//					find everythig between $ and \r\n
-//				store as length of string
-//				extract string
-//					find first \r\n
-//					grab length of string starting after \r\n
-//				take string and store in first empty space in array
-//
+func commandHandler(slc *[]string) error {
+	switch (*slc)[0] {
+	case "set":
+		set(slc)
+	case "get":
+		get(slc)
+	default:
+		responder("No such command " + (*slc)[0] + ", try SET or GET")
+	}
+
+	return nil
+}
+
+func set(*[]string) {
+	// check that there are three elements
+	//		throw error if false
+	//
+}
+func get(*[]string) {}
+
 func main() {
 	// s, _ := receiver()
 	// fmt.Printf("%v", s)
-	parser(input)
-}
+	input, err := receiver()
+	if err != nil {
+		fmt.Println("receiver error:", err)
+	}
 
-/*
-command: set a 21
-(set key `a` to value `21`)
-RESP:
-*3\r\n$3\r\nset\r\n$1\r\na\r\n$2\r\n21\r\n
-*/
+	s, err := parser(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = commandHandler(&s)
+	if err != nil {
+		fmt.Println("commanderHandler():", err)
+	}
+}
