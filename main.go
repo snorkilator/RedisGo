7e6df0b238e4c62d577562696dc984c6b66ef8e1
daniel <daniel@rudenberg.ca>
2021-11-24 21:33:35 -0500

    jon's suggestions

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func sendErr(s error, conn net.Conn) error {
 	return nil
 }
 
-// Parse takes RESP array of bulk strings, and outputs a slice of []bytes containing the elements of the array
+// parse takes RESP array of bulk strings, and outputs a slice of []bytes containing the elements of the array
 func parse(b []byte) ([][]byte, error) {
 	s := string(b) // get rid of this unnecessary copy operation, don't want to have to copy large data if it is unnecessary to do so
 	var parsed [][]byte
@@ -110,7 +110,7 @@ func parse(b []byte) ([][]byte, error) {
 	return parsed, nil
 }
 
-// GetLen is a utility for parse(). It finds the indicator of length for arrays and bulk strings within arrays. Returns length as int as well as the updated index.
+// getLen is a utility for parse(). It finds the indicator of length for arrays and bulk strings within arrays. Returns length as int as well as the updated index.
 func getLen(i int, s *string) (int, int, error) {
 	beginStr := i
 	var endofNum int
@@ -135,7 +135,7 @@ func getLen(i int, s *string) (int, int, error) {
 	return len, i + 3, nil
 }
 
-// HandleCommand executes command with listed arguments and passes response from command back to caller.
+// handleCommand executes command with listed arguments and passes response from command back to caller.
 func handleCommand(slc *[][]byte) (resp [][]byte, err error) {
 	switch string((*slc)[0]) {
 	case "set":
@@ -150,7 +150,7 @@ func handleCommand(slc *[][]byte) (resp [][]byte, err error) {
 	return
 }
 
-// Set puts input into database.
+// set puts input into database.
 func set(slc *[][]byte) ([][]byte, error) {
 	resp := [][]byte{}
 	len := len(*slc)
@@ -163,7 +163,7 @@ func set(slc *[][]byte) ([][]byte, error) {
 	return resp, nil
 }
 
-// Get collects and outputs requested data.
+// get collects and outputs requested data.
 func get(slc *[][]byte) ([][]byte, error) {
 	len := len(*slc)
 	resp := [][]byte{}
@@ -178,7 +178,7 @@ func get(slc *[][]byte) ([][]byte, error) {
 	return resp, nil
 }
 
-// Respond sends information back to client.
+// respond sends information back to client.
 func respond(slc [][]byte, conn net.Conn) error {
 	toSend := fmtData(slc)
 	fmt.Println("sent:", string(toSend))
@@ -192,7 +192,7 @@ func respond(slc [][]byte, conn net.Conn) error {
 	return err
 }
 
-// FmtData formats input as RESP array of bulk strings.
+// fmtData formats input as RESP array of bulk strings.
 func fmtData(slc [][]byte) []byte {
 	delim := `\r\n`
 	elCount := fmt.Sprint(len(slc))
--- main_test.go
+++ main_test.go
@@ -1,54 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 	"reflect"
 	"testing"
 )
 
-func TestFmtData(T *testing.T) {
-	var input [][]byte = [][]byte{[]byte("get"), []byte("a")}
+func TestFmtData(t *testing.T) {
+	input := [][]byte{[]byte("get"), []byte("a")}
 
-	var want []byte = []byte(`*2\r\n$3\r\nget\r\n$1\r\na\r\n`)
+	want := []byte(`*2\r\n$3\r\nget\r\n$1\r\na\r\n`)
 	got := fmtData(input)
 
 	if !reflect.DeepEqual(got, want) {
-		T.Fatalf("got %v, want %v", string(got), string(want))
+		t.Fatalf("got %v, want %v", string(got), string(want))
 	}
 }
 
-func TestHandleCommandError(T *testing.T) {
+func TestHandleCommandError(t *testing.T) {
 	// test error for incorrect command
 	input := [][]byte{[]byte("net")}
 	_, err := handleCommand(&input)
 	if err.Error() != fmt.Sprintf("handleCommand: no such command %s, try set or get", input[0]) {
-		T.Fatalf("handleCommand should throw error")
+		t.Fatalf("handleCommand should throw error")
 	}
 }
 
-func TestGetError(T *testing.T) {
+func TestGetError(t *testing.T) {
 	// error when there are too many
 	tooManySlc := [][]byte{[]byte("get"), []byte("get"), []byte("get")}
 	_, err := get(&tooManySlc)
 	if err.Error() != "get: wrong number of arguments. want 2 but got 3" {
-		T.Fatalf("get should return error for too many inputs")
+		t.Fatalf("get should return error for too many inputs")
 	}
 
 	tooFewSlc := [][]byte{[]byte("get")}
 	_, err = get(&tooFewSlc)
 	if err.Error() != "get: wrong number of arguments. want 2 but got 1" {
-		T.Fatalf("get should return error for too few inputs")
+		t.Fatalf("get should return error for too few inputs")
 	}
 
 	// error when key is not found in database
 	keyNotPresent := [][]byte{[]byte("get"), []byte("a")}
 	_, err = get(&keyNotPresent)
 	if err.Error() != fmt.Sprintf(`get: "%v" does not exist in database`, keyNotPresent[1]) {
-		T.Fatalf("get should return error: no key found in database")
+		t.Fatalf("get should return error: no key found in database")
 	}
 }
 
-func TestGetSetHappy(T *testing.T) {
+func TestGetSetHappy(t *testing.T) {
 	db = make(map[string][]byte)
 	setA := [][]byte{[]byte("set"), []byte("a"), []byte("b")}
 	getA := [][]byte{[]byte("get"), []byte("a")}
@@ -56,41 +58,41 @@ func TestGetSetHappy(T *testing.T) {
 
 	ok, err := set(&setA)
 	if err != nil {
-		T.Fatalf("unexpected error: %v", err)
+		t.Fatalf("unexpected error: %v", err)
 	}
 	if !reflect.DeepEqual(ok, expectOk) {
-		T.Fatalf(`expected "OK", got %v`, ok)
+		t.Fatalf(`expected "OK", got %v`, ok)
 	}
 
 	expected := [][]byte{[]byte("b")}
 	result, err := get(&getA)
 	if err != nil {
-		T.Fatalf("unexpected error: %v", err)
+		t.Fatalf("unexpected error: %v", err)
 	}
 	if !reflect.DeepEqual(result, expected) {
-		T.Fatalf("was not able to set and get")
+		t.Fatalf("was not able to set and get")
 	}
 
 }
 
-func TestSetError(T *testing.T) {
+func TestSetError(t *testing.T) {
 	// error when there are too many elements
 	tooManySlc := [][]byte{[]byte("set"), []byte("set"), []byte("set"), []byte("set")}
 	_, err := set(&tooManySlc)
 	if err.Error() != "set: wrong number of arguments. want 3 but got 4" {
-		T.Fatalf("get should return error if too many elements")
+		t.Fatalf("get should return error if too many elements")
 	}
 
 	// error when there are too few elements
 	tooFewSlc := [][]byte{[]byte("get")}
 	_, err = set(&tooFewSlc)
 	if err.Error() != "set: wrong number of arguments. want 3 but got 1" {
-		T.Fatalf("get should return error for too few inputs")
+		t.Fatalf("get should return error for too few inputs")
 	}
 
 }
 
-func TestParseHappy(T *testing.T) {
+func TestParseHappy(t *testing.T) {
 	tc := []struct {
 		input  []byte
 		expect [][]byte
@@ -108,11 +110,33 @@ func TestParseHappy(T *testing.T) {
 	for _, e := range tc {
 		got, err := parse(e.input)
 		if err != nil {
-			T.Fatalf("unexpected error")
+			t.Fatalf("unexpected error")
 		}
 		if !reflect.DeepEqual(got, e.expect) {
-			T.Fatalf("got %v want %v", got, e.expect)
+			t.Fatalf("got %v want %v", got, e.expect)
 		}
 
+	}
+}
+
+func TestServerAccept(t *testing.T) {
+	go main()
+	conn, err := net.Dial("tcp", "localhost:3333")
+	if err != nil {
+		t.Fatalf("error dialing")
+	}
+	defer conn.Close()
+
+	c := make(chan []byte)
+	go func(conn net.Conn, msg chan []byte) {
+		for {
+			buf := bufio.NewReader(conn)
+			data, _ := buf.ReadBytes(10)
+			c <- data
+		}
+	}(conn, c)
+	_, err = conn.Write([]byte(`*3\r\n$3\r\nset\r\n$1\r\na\r\n$1\r\nb\r\n`))
+	if err != nil {
+		t.Fatalf("error sending data")
 	}
 }
